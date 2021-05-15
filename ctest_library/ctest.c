930e02f1d0b9aa525b903ea32bb99ec3b132e301
ArturAssisComp <arturassiscomp@gmail.com>
2021-05-15 18:28:28 -0300

    Add the prototypes of the functions and implement a very basic print_result for debugging

---
+++ ctest_library/ctest.c
@@ -0,0 +1,23 @@
+/**
+ * Author: Artur Assis Alves
+ * Date: 15/05/2021
+ * Title: CTEST
+ */
+
+/**
+ * Description:
+ *
+ */
+
+
+//Includes:
+#include "ctest.h"
+#include "assert.h"
+#include <stdio.h>
+
+
+//Function definitions:
+void print_result()
+{
+	printf("The result of the function is: %s", global_result.result_message);
+}
---
+++ ctest_library/ctest.h
@@ -0,0 +1,10 @@
+#ifndef CTEST_H
+#define CTEST_H
+
+//Declaration of functions:
+void print_result();
+void start_suite();
+void end_suite();
+void start_module();
+void end_module();
+#endif
