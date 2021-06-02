d255c0f5e5723fda1407d0a3106a55bdbd5cdbd0
ArturAssisComp <arturassiscomp@gmail.com>
2021-06-02 11:16:35 -0300

    Implement the function end_suite

--- ctest_library/ctest.c
+++ ctest_library/ctest.c
@@ -24,13 +24,11 @@
 
 //Global Variables:
 char progress[MAX_RESULTS + 1] = "\0";
-char module_progress[MAX_RESULTS + 1] = "\0";
 char suite_progress[MAX_RESULTS + 1] = "\0";
+char suite_name[MAX_NAME], module_name[MAX_NAME];
 int num_of_suite_results   = 0;
-int num_of_module_results  = 0;
 int num_of_results         = 0;
 int num_of_suite_fails  = 0;
-int num_of_module_fails = 0;
 int num_of_fails        = 0;
 
 //Function definitions:
@@ -99,6 +97,9 @@ void start_suite(char *name, char *description, char *func_ids[])
 /**
  * Description: This function starts a new suite of tests. It reads the name of
  * the suite, the description and the IDs of the functions that will be tested.
+ * 	The body of the test suite must be nested between a start_suite() and 
+ * end_suite().
+ *
  *
  * Input: (char *) name --> The name of the suite;
  *        (char *) description --> The description of the suite;
@@ -114,15 +115,13 @@ void start_suite(char *name, char *description, char *func_ids[])
 {
 	char *thick_line = "================================================================================"; // Repeat '=' 80 times.
 	char *thin_line = "--------------------------------------------------------------------------------"; // Repeat '=' 80 times.
-	char suite_name[MAX_NAME], suite_description[MAX_DESCRIPTION], id[MAX_NAME], *aux_str, *merged_str;
-	int num_of_ids = -1, n, line_length = 80;
+	char suite_description[MAX_DESCRIPTION], *aux_str, *merged_str;
+	int n, line_length = 80;
 
 	//Reset global variables related to the suite:
-	num_of_suite_results = 0;
-	num_of_suite_fails   = 0;
-	num_of_results = num_of_suite_results;
-	num_of_fails   = num_of_suite_fails;
-	progress[0] = suite_progress[0] = '\0';
+	num_of_results = 0;
+	num_of_fails   = 0;
+	progress[0]    = '\0';
 	
 	//Initialize variables:
 	n = snprintf(suite_name, MAX_NAME, name);
@@ -138,10 +137,6 @@ void start_suite(char *name, char *description, char *func_ids[])
 		fprintf(stderr, "Error while initializing suite_description.\n");
 		exit(EXIT_FAILURE);
 	}
-
-	//Count the number of ids:
-	if (func_ids != NULL)
-		while(func_ids[++num_of_ids] != NULL);
 
 	//Print the starting informations:
 	n = fprintf(stdout, "%s\n|    START SUITE: %-60s |\n%s\n", thick_line, suite_name, thin_line);
@@ -162,7 +157,7 @@ void start_suite(char *name, char *description, char *func_ids[])
 
 	merged_str = merge_str(func_ids, ", ", ".");
 	aux_str = break_line(merged_str, strlen(merged_str), "|    FUNCTIONS COVERED: ", "|    ", " |", line_length);
-	n = fprintf(stdout, "%s%s\n", aux_str, thick_line);
+	n = fprintf(stdout, "%s%s\n\n", aux_str, thick_line);
 	if (n < 0)
 	{
 		fprintf(stderr, "Error while printing the suite functions ids.\n");
@@ -173,4 +168,53 @@ void start_suite(char *name, char *description, char *func_ids[])
 }
 
 
+void end_suite()
+/**
+ * Description: This function ends a suite, printing its summary of results.
+ * 
+ * Input: (void)
+ *
+ * Output: (void)
+ *
+ * Time/Space Complexity: -
+ */
+{
+	char *thick_line = "================================================================================"; // Repeat '=' 80 times.
+	char *thin_line = "--------------------------------------------------------------------------------"; // Repeat '=' 80 times.
+	int n;
+
+	//Print the starting informations:
+	n = fprintf(stdout, "\n\n%s\n|    SUMMARY OF THE SUITE: %-51s |\n%s\n", thick_line, suite_name, thin_line);
+	if (n < 0)
+	{
+		fprintf(stderr, "Error while printing the suite name.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	n = fprintf(stdout, "|    -> TOTAL OF CASES: %-54d |\n%s\n", num_of_results, thin_line);
+	if (n < 0)
+	{
+		fprintf(stderr, "Error while printing the suite name.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	n = fprintf(stdout, "|    -> SUCCESSES: %-59d |\n%s\n", num_of_results - num_of_fails, thin_line);
+	if (n < 0)
+	{
+		fprintf(stderr, "Error while printing the suite name.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	n = fprintf(stdout, "|    -> FAILS: %-63d |\n%s\n\n", num_of_fails, thin_line);
+	if (n < 0)
+	{
+		fprintf(stderr, "Error while printing the suite name.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	//Reset global variables related to the suite:
+	num_of_results = 0;
+	num_of_fails   = 0;
+	suite_name[0]  = '\0';
+}
 
--- ctest_library/ctest.h
+++ ctest_library/ctest.h
@@ -3,8 +3,8 @@
 
 //Declaration of functions:
 void print_result();
-void start_suite();
+void start_suite(char *name, char *description, char *func_ids[]);
 void end_suite();
-void start_module();
+void start_module(char *name, char *description, char *func_ids[]);
 void end_module();
 #endif
