601521f96543e45f219bb08a403440eff4fbc498
ArturAssisComp <arturassiscomp@gmail.com>
2021-06-09 18:33:34 -0300

    Implement naive versions of float and double almost equal asserts

--- ctest_library/assert.c
+++ ctest_library/assert.c
@@ -11,6 +11,7 @@
 //Includes:
 #include "ctest.h"
 #include <stdio.h>
+#include <math.h>
 
 
 //Definitions of global Variables:
@@ -50,7 +51,7 @@ void assert_int_equal(int target, int ref)
 		//Generate the error message:
 		n = snprintf(global_result.result_message, 
 					MAX_CHARS,
-					"The target value is not equal to the reference value.\n %d != %d (target != reference)\n", 
+					"The target value is not equal to the reference value.\n--> %d != %d (target != reference)\n", 
 					target, 
 					ref
 			    );
@@ -102,7 +103,7 @@ void assert_integer_equal(integer target, integer ref)
 		//Generate the error message:
 		n = snprintf(global_result.result_message, 
 					MAX_CHARS,
-					"The target value is not equal to the reference value.\n %lld != %lld (target != reference)\n", 
+					"The target value is not equal to the reference value.\n--> %lld != %lld (target != reference)\n", 
 					target, 
 					ref
 			    );
@@ -153,7 +154,7 @@ void assert_char_equal(char target, char ref)
 		//Generate the error message:
 		n = snprintf(global_result.result_message, 
 					MAX_CHARS,
-					"The target value is not equal to the reference value.\n %c != %c (target != reference)\n", 
+					"The target value is not equal to the reference value.\n--> %c != %c (target != reference)\n", 
 					target, 
 					ref
 			    );
@@ -171,6 +172,168 @@ void assert_char_equal(char target, char ref)
 	//Reset global variable:
 	reset_global_result();
 }
+
+
+
+void assert_float_almostEqual(float target, float ref, float max_diff)
+/**
+ * Description: This function asserts if the floats 'target' and 'ref' are 
+ * almost equal. The maximum difference allowed must be less than (<) the 
+ * positive value (> 0) max_diff.
+ *
+ * WARNING: the maximum precision sugested is 0.00001. Thus, max_diff should be
+ * a multiple of 0.00001. This is a very naive implementation for floating point 
+ * comparison because it uses absolute error! 
+ *
+ *
+ * Input: (float) target --> Value that will be compared with the reference.
+ *        (float) ref --> The reference value.
+ *        (float) max_diff --> Upper bound for the module of the difference 
+ *        between target and ref in order to consider target == ref. For 
+ *        example, if the user wants the precision to be 2 decimal cases, 
+ *        then max_diff must be 0.01.
+ *
+ * Output: (void)
+ *
+ * Time Complexity: O(1)
+ *
+ * Space Complexity: O(1)
+ */
+{
+	int n;
+	float diff = fabsf(target - ref);
+	float min = 0.00001;
+
+	//Check max_diff:
+	if (!(max_diff >= min))
+	{
+		fprintf(stderr, "ERROR: The third argument must be greater than %f.\n", min);
+		exit(EXIT_FAILURE);
+	}
+
+
+	if (diff < max_diff)
+	{
+		//Successful test:
+		global_result.was_successful = TRUE;
+		global_result.result_message[0] = '\0';
+	}else
+	{
+		//Failed test:
+		global_result.was_successful = FALSE;
+
+		//Generate the error message:
+		n = snprintf(global_result.result_message, 
+					MAX_CHARS,
+					"The target value is not almost equal to the reference value.\n--> %f != %f (target != reference)\n--> |target - reference| (%f) >= max diff (%f)\n", 
+					target, 
+					ref,
+					diff,
+					max_diff
+			    );
+			   
+		if (n < 0) //Error creating the result message.
+		{
+			fprintf(stderr, "Error while generating the result message.\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	//Print the results:
+	print_result();
+	
+	//Reset global variable:
+	reset_global_result();
+}
+
+
+void assert_double_almostEqual(double target, double ref, double max_diff)
+/**
+ * Description: This function asserts if the doubles 'target' and 'ref' are 
+ * almost equal. The maximum difference allowed is must be less than (<) the 
+ * positive value (> 0) of max_diff.
+ *
+ *
+ * WARNING: the maximum precision sugested is 0.000000000001. Thus, max_diff should be
+ * a multiple of 0.000000000001. This is a very naive implementation for floating point 
+ * comparison because it uses absolute error! 
+ *
+ * Input: (double) target --> Value that will be compared with the reference.
+ *        (double) ref --> The reference value.
+ *        (double) max_diff --> Upper bound for the module of the difference 
+ *        between target and ref in order to consider target == ref. For 
+ *        example, if the user wants the precision to be 2 decimal cases, 
+ *        then max_diff must be 0.01.
+ *
+ * Output: (void)
+ *
+ * Time Complexity: O(1)
+ *
+ * Space Complexity: O(1)
+ */
+{
+	int n;
+	double diff = fabs(target - ref);
+	double min = 0.000000000001;
+
+	//Check max_diff:
+	if (!(max_diff >= min))
+	{
+		fprintf(stderr, "ERROR: The third argument must be greater than %.13lf.\n", min);
+		exit(EXIT_FAILURE);
+	}
+
+
+	if (diff < max_diff)
+	{
+		//Successful test:
+		global_result.was_successful = TRUE;
+		global_result.result_message[0] = '\0';
+	}else
+	{
+		//Failed test:
+		global_result.was_successful = FALSE;
+
+		//Generate the error message:
+		n = snprintf(global_result.result_message, 
+					MAX_CHARS,
+					"The target value is not almost equal to the reference value.\n--> %.13lf != %.13lf (target != reference)\n--> |target - reference| (%.13lf) >= max diff (%.13lf)\n", 
+					target, 
+					ref,
+					diff,
+					max_diff
+			    );
+			   
+		if (n < 0) //Error creating the result message.
+		{
+			fprintf(stderr, "Error while generating the result message.\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	//Print the results:
+	print_result();
+	
+	//Reset global variable:
+	reset_global_result();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Auxiliary Function definitions:
 void reset_global_result()
--- ctest_library/assert.h
+++ ctest_library/assert.h
@@ -8,6 +8,7 @@
 #define FALSE 0
 #define MAX_CHARS 256 //The maximum number of chars for a message.
 
+
 //Definitions of structs:
 typedef struct
 {
@@ -26,6 +27,13 @@ extern result global_result;
 void assert_int_equal(int target, int ref);
 void assert_integer_equal(integer target, integer ref);
 void assert_char_equal(char target, char ref);
+void assert_float_almostEqual(float target, float ref, float max_diff);
+void assert_double_almostEqual(double target, double ref, double max_diff);
+
+/*
+ * List to implement:
+ * unsigned long long int
+ * */
 
 //Auxiliary functions:
 void reset_global_result();
