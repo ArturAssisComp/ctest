98a5d60465fdc9291ace3b976eefb5c6f6cec750
Artur Assis Alves <arturassiscomp@gmail.com>
2021-12-23 20:16:23 -0300

    Implement macro ASSERT_UNSIGNED_INTEGER_ARRAY_IS_PARTIAL_PERMUTATION

--- ctest_library/assert/std_assert_macros.h
+++ ctest_library/assert/std_assert_macros.h
@@ -218,7 +218,7 @@ do {;} while (0)
  * Macro Name: ASSERT_UNSIGNED_INTEGER_ARRAY_NOT_PERMUTATION
  * Description: This macro makes it possible to use arrays of any unsigned type 
  * as parameters. It casts each element to unsigned_integer and then calls the
- * function assert_unsigned_integerArray_permutation.
+ * function assert_unsigned_integerArray_notPermutation.
  * 	The assert macro must be ended with a semicolon.
  * 	To pass literal arrays as arguments, the following conventions must be 
  * used: ((unsigned <type>[]){a1, a2, ..., an}) 
@@ -255,10 +255,50 @@ do {;} while (0)
 do {;} while (0)
 
 
+/**
+ * Macro Name: ASSERT_UNSIGNED_INTEGER_ARRAY_IS_PARTIAL_PERMUTATION
+ * Description: This macro makes it possible to use arrays of any unsigned type 
+ * as parameters. It casts each element to unsigned_integer and then calls the
+ * function assert_unsigned_integerArray_isPartialPermutation.
+ * 	The assert macro must be ended with a semicolon.
+ * 	To pass literal arrays as arguments, the following conventions must be 
+ * used: ((unsigned <type>[]){a1, a2, ..., an}) 
+ * 	-Replace <type> with the desired type of each element of the array and 
+ * replace ai with each respective element.
+ * 	-The outer perenteses are necessary.
+ * Ex:
+ * 	ASSERT_UNSIGNED_INTEGER_ARRAY_IS_PARTIAL_PERMUTATION(((unsigned char[]){12, 3}),2,((unsigned int[]){1, 2, 3}),3, __LINE__, NULL);
+ *
+ * Input:
+ * 	TARGET      --> Array/pointer to any unsigned integer type.
+ * 	TARGET_SIZE --> The number of elements of TARGET.
+ * 	REFERENCE      --> Array/pointer to any unsigned integer type.
+ * 	REFERENCE_SIZE --> The number of elements of REFERENCE.
+ * 	LINE_NUMBER    --> The number of the line.
+ * 	CUSTOM_MESSAGE --> Custom message that will be printed if the assertion fails.
+ */
+#define ASSERT_UNSIGNED_INTEGER_ARRAY_IS_PARTIAL_PERMUTATION(TARGET, TARGET_SIZE, REFERENCE, REFERENCE_SIZE, LINE_NUMBER, CUSTOM_MESSAGE) \
+{\
+	unsigned_integer target[(TARGET_SIZE)];\
+	unsigned_integer reference[(REFERENCE_SIZE)];\
+\
+	/*Cast each element to unsigned_integer:*/\
+	size_t i;\
+	for(i = 0; i < (TARGET_SIZE); i++)\
+		target[i] = (unsigned_integer) (TARGET)[i];\
+\
+	for(i = 0; i < (REFERENCE_SIZE); i++)\
+		reference[i] = (unsigned_integer) (REFERENCE)[i];\
+\
+	/*Call the assert function:*/\
+	assert_unsigned_integerArray_isPartialPermutation(target, (TARGET_SIZE), reference, (REFERENCE_SIZE), (LINE_NUMBER), (CUSTOM_MESSAGE));\
+}\
+do {;} while (0)
+
+
 
 
 /*
-void assert_unsigned_integerArray_isPartialPermutation     (unsigned_integer target[], size_t target_size, unsigned_integer reference[], size_t reference_size, int line_number, char custom_message[]);
 void assert_unsigned_integerArray_notIsPartialPermutation  (unsigned_integer target[], size_t target_size, unsigned_integer reference[], size_t reference_size, int line_number, char custom_message[]);
 void assert_unsigned_integerArray_hasPartialPermutation    (unsigned_integer target[], size_t target_size, unsigned_integer reference[], size_t reference_size, int line_number, char custom_message[]);
 void assert_unsigned_integerArray_notHasPartialPermutation (unsigned_integer target[], size_t target_size, unsigned_integer reference[], size_t reference_size, int line_number, char custom_message[]);
